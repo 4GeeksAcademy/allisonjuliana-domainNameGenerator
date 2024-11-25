76bf737ff44cb9d2fe9dfa55c1c266acaf9658e7
allisonjuliana <162182668+allisonjuliana@users.noreply.github.com>
2024-11-25 19:54:05 +0000

    domainnamegenerator

--- src/app.js
+++ src/app.js
@@ -5,20 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let pronoun = ["yo", "tu", "esto"];
-let adj = [
-  "el más alto",
-  "el más bajito",
-  "el más fuerte",
-  "el más ágil",
-  "el más débil"
-];
-let noun = ["comerciante", "ciudadano", "atleta", "marido", "amante"];
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
 
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
-      console.log(`${pronoun[i]} ${adj[j]} ${noun[k]}.com`);
+      console.log(`${pronoun[i]}${adj[j]}${noun[k]}.com`);
     }
   }
 }
